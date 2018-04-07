0e4827344b8ceadb33ae038f410fb9e2039bff33
unknown <Aprendiz Sena@TUNDFPCGE7F358.FormacionSena.RED>
2018-04-07 13:31:06 -0500

    DemoView Tablas

--- DemoView2/DemoView2/MainPage.xaml.cs
+++ DemoView2/DemoView2/MainPage.xaml.cs
@@ -15,50 +15,50 @@ namespace DemoView2
 
         }
 
-        private void pickMusic_Changed(object sender, EventArgs e)
-        {
-            var elementoSeleccionado = pinkMusic.SelectedItem.ToString();
-            DisplayAlert("Musica", elementoSeleccionado, "Aceptar");
-        }
+        //private void pickMusic_Changed(object sender, EventArgs e)
+        //{
+        //    var elementoSeleccionado = pinkMusic.SelectedItem.ToString();
+        //    DisplayAlert("Musica", elementoSeleccionado, "Aceptar");
+        //}
 
-        private void btnSimular_Clicked(object sender, EventArgs e)
-        {
-            var progreso = progProceso.Progress;
-            if (progreso == 1)
-            {
-                progProceso.ProgressTo(.1, 250, Easing.Linear);
-            }
-            else
-            {
-                progProceso.ProgressTo(.1, 250, Easing.Linear);
-            }            
-        }
+        //private void btnSimular_Clicked(object sender, EventArgs e)
+        //{
+        //    var progreso = progProceso.Progress;
+        //    if (progreso == 1)
+        //    {
+        //        progProceso.ProgressTo(.1, 250, Easing.Linear);
+        //    }
+        //    else
+        //    {
+        //        progProceso.ProgressTo(.1, 250, Easing.Linear);
+        //    }            
+        //}
 
-        private void sbNombre_Pressed(object sender, EventArgs e)
-        {
-            DisplayAlert("Search Bar", "Estoy Buscando....", "Aceptar");
-        }
+        //private void sbNombre_Pressed(object sender, EventArgs e)
+        //{
+        //    DisplayAlert("Search Bar", "Estoy Buscando....", "Aceptar");
+        //}
 
-        private void sldEdad_Changed(object sender, EventArgs e)
-        {
-            lbEdad.Text = sldEdad.Value.ToString();
-        }
+        //private void sldEdad_Changed(object sender, EventArgs e)
+        //{
+        //    lbEdad.Text = sldEdad.Value.ToString();
+        //}
 
-        private void stpCantidad_Changed(object sender, EventArgs e)
-        {
-            lbCantidad.Text = this.stpCantidad.Value.ToString();
-        }
+        //private void stpCantidad_Changed(object sender, EventArgs e)
+        //{
+        //    lbCantidad.Text = this.stpCantidad.Value.ToString();
+        //}
 
-        private void swiDec_Toggle(object sender, EventArgs e)
-        {
-            if (this.swiDec.IsToggled)
-            {
-                DisplayAlert("Mensaje", "Está Activo", "Aceptar");
-            }
-            else
-            {
-                DisplayAlert("Mensaje", "Está Activo", "Aceptar");
-            }
-        }
+        //private void swiDec_Toggle(object sender, EventArgs e)
+        //{
+        //    if (this.swiDec.IsToggled)
+        //    {
+        //        DisplayAlert("Mensaje", "Está Activo", "Aceptar");
+        //    }
+        //    else
+        //    {
+        //        DisplayAlert("Mensaje", "Está Activo", "Aceptar");
+        //    }
+        //}
     }
 }
